e879b403c5e473c01e82684fcc97f890bc1ac7bb
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-03 15:29:53 +0300

    react-bootstrap setup, added header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-      <h1>Welcom to mern eCommerce app</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcom to mern eCommerce app</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
