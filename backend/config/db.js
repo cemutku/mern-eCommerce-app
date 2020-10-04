23ff799ede80447338ac7243ae7d0a6757eacabb
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-04 12:40:32 +0300

    added user, product and order models

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDb = async () => {
       useCreateIndex: true,
     });
 
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 
 import connectDb from './config/db.js';
 import products from './data/products.js';
 
+// define environment variables
 dotenv.config();
 
+// start connection
 connectDb();
 
+// create api server
 const app = express();
 
 app.get('/', (req, res) => {
@@ -27,5 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
