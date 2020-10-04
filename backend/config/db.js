53bf0a5ad9e8dfbcf4498e8e41eb317b32690287
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-04 12:19:25 +0300

    MongoDB setup - added mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+import connectDb from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDb();
 
 const app = express();
 
