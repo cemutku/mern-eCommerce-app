965a9ddc24f6db425c6bb2a9798b7c0e655ec186
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-06 19:31:13 +0300

    Added cart reducers, actions and localStorage cart setup

---
+++ frontend/src/actions/cart-actions.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { CART_ADD_ITEM } from '../constants/cart-constants';
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+
+  // add cart items to local storage
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,24 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/product-reducers';
+import { cartReducer } from './reducers/cart-reducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+// get cart items from localStorage if exist
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+  },
+};
 
 const middleware = [thunk];
 
