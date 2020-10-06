d438e9e8408144947a25775d1c7b83902516c059
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-06 22:14:49 +0300

    Added useDispatch and useEffect hooks to the CartScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,7 +16,7 @@ import Message from '../components/Message';
 import { listProductDetails } from '../actions/product-actions';
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
 
