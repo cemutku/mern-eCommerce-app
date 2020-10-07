58cd012bc33872c21121c43d3156481e5a6f8b1a
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-07 21:10:26 +0300

    Added user registration process (user registration route and password encryption before save)

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/user-controller.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/user-controller.js';
 import { protect } from '../middleware/auth-middleware.js';
 
 const router = express.Router();
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
 
