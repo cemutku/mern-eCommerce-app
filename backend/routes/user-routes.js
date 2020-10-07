549cdc71d1056c0125a66e9a2f63679e111d1772
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-07 20:56:11 +0300

    Added authentication middleware and user profile handler

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { authUser } from '../controllers/user-controller.js';
+import { authUser, getUserProfile } from '../controllers/user-controller.js';
+import { protect } from '../middleware/auth-middleware.js';
 
 const router = express.Router();
 
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
