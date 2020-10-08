ac5496f4187100af7947f3892c9a1ad833e25791
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-08 20:52:57 +0300

    Added update user profile process and route

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -76,4 +76,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+// @description     Update user profile
+// @route           PUT /api/users/profile
+// @access          Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.send(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -3,6 +3,7 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/user-controller.js';
 import { protect } from '../middleware/auth-middleware.js';
 
@@ -10,6 +11,9 @@ const router = express.Router();
 
 router.route('/').post(registerUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
