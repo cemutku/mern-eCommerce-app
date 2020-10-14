684094c2c82207ba467a010021c80c66f2fe454c
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-15 00:59:03 +0300

    Add create product and update product functions
    Added routes for creating(POST) and updating(PUT) products.
    
    Sample.jpg is added for create product template.

--- backend/routes/product-routes.js
+++ backend/routes/product-routes.js
@@ -3,15 +3,18 @@ import {
 	getProducts,
 	getProductById,
 	deleteProduct,
+	createProduct,
+	updateProduct,
 } from '../controllers/product-controller.js';
 import { protect, isAdmin } from '../middleware/auth-middleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
 router
 	.route('/:id')
 	.get(getProductById)
-	.delete(protect, isAdmin, deleteProduct);
+	.delete(protect, isAdmin, deleteProduct)
+	.put(protect, isAdmin, updateProduct);
 
 export default router;
