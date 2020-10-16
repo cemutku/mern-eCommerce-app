f696d84d8f499d6ddf56f8ccaebf3b3a188df0e2
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-17 00:01:58 +0300

    Add file upload endpoint
    Added multer for file upload process.
    Created upload route and multer configuration.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -6,6 +7,7 @@ import productRoutes from './routes/product-routes.js';
 import userRoutes from './routes/user-routes.js';
 import orderRoutes from './routes/order-routes.js';
 import paymentRoutes from './routes/payment-routes.js';
+import uploadRoutes from './routes/upload-routes.js';
 import { notFound, errorHandler } from './middleware/error-middleware.js';
 
 // define environment variables
@@ -28,6 +30,11 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/payments', paymentRoutes);
+app.use('/api/upload', uploadRoutes);
+
+// make uploads folder static
+const __dirname = path.resolve(); // for es modules __dirname is only available if we're using commonjs
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // not found middleware
 app.use(notFound);
