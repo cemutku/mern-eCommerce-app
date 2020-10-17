fb66207cd4c822b341dfe5d8f5e2ba6b514120e6
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-18 00:09:28 +0300

    Add create review endpoint
    Created create product review function.
    Created product review route.
    User associated with the review.
    Added morgan package for logging.

--- backend/routes/product-routes.js
+++ backend/routes/product-routes.js
@@ -5,12 +5,14 @@ import {
 	deleteProduct,
 	createProduct,
 	updateProduct,
+	createProductReview,
 } from '../controllers/product-controller.js';
 import { protect, isAdmin } from '../middleware/auth-middleware.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 router
 	.route('/:id')
 	.get(getProductById)
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
 import connectDb from './config/db.js';
 import productRoutes from './routes/product-routes.js';
 import userRoutes from './routes/user-routes.js';
@@ -18,6 +19,10 @@ connectDb();
 
 // create api server
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 // accept json data in the body
 app.use(express.json());
