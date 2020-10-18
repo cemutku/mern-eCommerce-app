e63f823373f7dd782a3b842e21b476fec677e1e6
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-18 10:04:31 +0300

    Add product review section to the product screen
    Created product review actions and reducer.
    Product review form and its functions added to the product screen.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
 	productDeleteReducer,
 	productCreateReducer,
 	productUpdateReducer,
+	productReviewCreateReducer,
 } from './reducers/product-reducers';
 import { cartReducer } from './reducers/cart-reducers';
 import {
@@ -33,6 +34,7 @@ const reducer = combineReducers({
 	productDelete: productDeleteReducer,
 	productCreate: productCreateReducer,
 	productUpdate: productUpdateReducer,
+	productReviewCreate: productReviewCreateReducer,
 	cart: cartReducer,
 	userLogin: userLoginReducer,
 	userRegister: userRegisterReducer,
