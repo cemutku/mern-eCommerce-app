50200f1541a7fc61a63178d1fd75ff7ea6f5586e
cemutku <33598193+cemutku@users.noreply.github.com>
2020-10-20 00:09:11 +0300

    Add product pagination
    Product controller updated for pagination feature.
    Added page size and page number properties.
    Added new routes for page number property.
    Added page number parameter to the list products action.
    Added pages and page parameters to the product list reducer.
    Created Paginate component.
    Added Paginate component to the home screen and the product list screen.

--- frontend/src/actions/product-actions.js
+++ frontend/src/actions/product-actions.js
@@ -23,13 +23,17 @@ import {
 } from '../constants/product-constants';
 
 // action creators
-export const listProducts = (keyword = '') => async (dispatch) => {
+export const listProducts = (keyword = '', pageNumber = '') => async (
+	dispatch
+) => {
 	try {
 		dispatch({
 			type: PRODUCT_LIST_REQUEST,
 		});
 
-		const { data } = await axios.get(`/api/products?keyword=${keyword}`);
+		const { data } = await axios.get(
+			`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`
+		);
 
 		dispatch({
 			type: PRODUCT_LIST_SUCCESS,
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,19 +4,23 @@ import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import Paginate from '../components/Paginate';
 import { listProducts } from '../actions/product-actions';
 
 const HomeScreen = ({ match }) => {
 	const keyword = match.params.keyword;
 
+	// if it does not have a page number, set it to 1
+	const pageNumber = match.params.pageNumber || 1;
+
 	const dispatch = useDispatch();
 
 	const productList = useSelector((state) => state.productList);
-	const { loading, error, products } = productList;
+	const { loading, error, products, page, pages } = productList;
 
 	useEffect(() => {
-		dispatch(listProducts(keyword));
-	}, [dispatch, keyword]);
+		dispatch(listProducts(keyword, pageNumber));
+	}, [dispatch, keyword, pageNumber]);
 
 	return (
 		<>
@@ -26,13 +30,20 @@ const HomeScreen = ({ match }) => {
 			) : error ? (
 				<Message variant='danger'>{error}</Message>
 			) : (
-				<Row>
-					{products.map((product) => (
-						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-							<Product product={product} />
-						</Col>
-					))}
-				</Row>
+				<>
+					<Row>
+						{products.map((product) => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+					</Row>
+					<Paginate
+						pages={pages}
+						page={page}
+						keyword={keyword ? keyword : ''}
+					/>
+				</>
 			)}
 		</>
 	);
